db7985b279c7038e0533d88306892d5ab17b6dea
russ whelchel <foolofshadows@gmail.com>
2018-03-13 12:33:35 -0500

    Initial Commit

---
+++ PTVN Builder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PTVN Builder
+//
+//  Created by Fool on 3/13/18.
+//  Copyright © 2018 Fool. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PTVN Builder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PTVN Builder
+//
+//  Created by Fool on 3/13/18.
+//  Copyright © 2018 Fool. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
