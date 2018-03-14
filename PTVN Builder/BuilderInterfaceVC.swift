99d801be0a76b900782dfc92d3c9e75cf11cdbaa
russ whelchel <foolofshadows@gmail.com>
2018-03-14 12:46:19 -0500

    Finished initial build, still needs bug testing and tweaking, but should be mostly usable.

---
+++ PTVN Builder/BuilderInterfaceVC.swift
@@ -0,0 +1,95 @@
+//
+//  BuilderInterfaceVC.swift
+//  PTVN Builder
+//
+//  Created by Fool on 3/13/18.
+//  Copyright © 2018 Fool. All rights reserved.
+//
+
+import Cocoa
+
+class BuilderInterfaceVC: NSViewController {
+
+    @IBOutlet weak var visitTimeView: NSTextField!
+    @IBOutlet weak var visitDayView: NSPopUpButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    
+    @IBAction func processButton(_ sender: AnyObject) {
+        //Get the clipboard to process
+        let pasteBoard = NSPasteboard.general
+        guard let theText = pasteBoard.string(forType: NSPasteboard.PasteboardType(rawValue: "public.utf8-plain-text")) else { return }
+        
+        //Make sure the clipboard contents are from PF
+        if !theText.contains("Flowsheets") {
+            //Create an alert to let the user know the clipboard doesn't contain
+            //the correct PF data
+            print("You broke it!")
+            //After notifying the user, break out of the program
+            let theAlert = NSAlert()
+            theAlert.messageText = "It doesn't look like you've copied the correct bits out of Practice Fusion.\nPlease try again or click the help button for complete instructions.\nIf the problem continues, please contact the administrator."
+            theAlert.beginSheetModal(for: self.view.window!) { (NSModalResponse) -> Void in
+                let returnCode = NSModalResponse
+                print(returnCode)}
+        }
+        
+        //Make sure the diagnosis are set to ICD-10 format
+        if !theText.contains("ICD-10") {
+            //Create an alert to let the user know the diagnoses are not set to ICD10
+            print("Not set to ICD10")
+            //After notifying the user, break out of the program
+            let theAlert = NSAlert()
+            theAlert.messageText = "It appears Practice Fusion is not set to show ICD-10 diagnoses codes.  Please set the Show by option in the Diagnoses section to ICD-10 and try again."
+            theAlert.beginSheetModal(for: self.view.window!) { (NSModalResponse) -> Void in
+                _ = NSModalResponse
+            }
+        }
+        
+        //Create a ChartData struct with the clipboard data
+        let currentData = ChartData(chartData: theText)
+        
+        //Get the info from the date scheduled popup menu
+        let ptVisitDate = visitDayView.indexOfSelectedItem
+        
+        //Where to save the file
+        var saveLocation = "Desktop"
+        switch ptVisitDate {
+        case 0:
+            saveLocation = "WPCMSharedFiles/zDoctor Review/06 Dummy Files"
+        case 1...4:
+            saveLocation = "WPCMSharedFiles/zruss Review/Tomorrows Files"
+        default:
+            saveLocation = "Desktop"
+        }
+        
+        //Get current date and format it
+        let theCurrentDate = Date()
+        let formatter = DateFormatter()
+        formatter.dateStyle = DateFormatter.Style.short
+        
+        //Get the visit date
+        guard let visitDate = theCurrentDate.addingDays(ptVisitDate) else { return }
+        let internalVisitDate = formatter.string(from: visitDate)
+        let labelDateFormatter = DateFormatter()
+        labelDateFormatter.dateFormat = "yyMMdd"
+        let labelVisitDate = labelDateFormatter.string(from: visitDate)
+        
+        let finalResults = "\(currentData.ptName)\nDOB:  \(currentData.ptDOB)     Age: \(currentData.ptAge)\nDate: \(internalVisitDate)\n\n\(visitBoilerplateText)CURRENT MEDICATIONS:\n\(currentData.currentMeds)\n\nALLERGIES:\n\(currentData.allergies)\n\nPREVENTIVE CARE:\n\(currentData.preventiveCare)\n\nPAST MEDICAL HISTORY:\n\(medKey)\(currentData.pmh)\n\nPAST SURGICAL HISTORY:\n\(currentData.psh)\n\nNUTRITION:\n\(currentData.nutritionalHistory)\n\nSOCIAL HISTORY:\n\(currentData.socialHistory)\n\nFAMILY HISTORY:\n\(currentData.familyHistory)\n\nDIAGNOSES:\n\(currentData.diagnoses)"
+        
+        print(currentData.diagnoses)
+        finalResults.copyToPasteboard()
+        
+        let fileName = "\(visitTimeView.stringValue) \(getFileLabellingName(currentData.ptName)) PTVN \(labelVisitDate).txt"
+        print(fileName)
+        //Creates a file with the final output on the desktop
+        let ptvnData = finalResults.data(using: String.Encoding.utf8)
+        let newFileManager = FileManager.default
+        let savePath = NSHomeDirectory()
+        newFileManager.createFile(atPath: "\(savePath)/\(saveLocation)/\(fileName)", contents: ptvnData, attributes: nil)
+    }
+
+}
---
+++ PTVN Builder/ChartData.swift
@@ -0,0 +1,140 @@
+//
+//  ChartData.swift
+//  PTVN Builder
+//
+//  Created by Fool on 3/13/18.
+//  Copyright © 2018 Fool. All rights reserved.
+//
+
+import Foundation
+
+struct ChartData {
+    var chartData = String()
+    var ptName: String {return nameAgeDOB(chartData).0}
+    var ptDOB: String {return nameAgeDOB(chartData).2}
+    var ptAge: String {return nameAgeDOB(chartData).1}
+    //var fileName: String {return ""}
+    var currentMeds: String {return chartData.simpleRegExMatch(Regexes.medications.rawValue).cleanTheTextOf(medBadBits).addCharacterToBeginningOfEachLine("- ")}
+    var diagnoses: String {return chartData.simpleRegExMatch(Regexes.diagnoses.rawValue).cleanTheTextOf(dxBadBits)}
+    var allergies: String {return chartData.simpleRegExMatch(Regexes.allergies.rawValue).cleanTheTextOf(basicAllergyBadBits)}
+    var nutritionalHistory: String { var theRegex = ""
+                                    if chartData.contains("Developmental history") {
+                                    theRegex = Regexes.nutrition1.rawValue
+                                    } else { theRegex = Regexes.nutrition2.rawValue}
+                                    return chartData.simpleRegExMatch(theRegex).cleanTheTextOf(nutritionBadBits)}
+    var socialHistory: String {return chartData.simpleRegExMatch(Regexes.social.rawValue).cleanTheTextOf(socialBadBits)}
+    var familyHistory: String {return chartData.simpleRegExMatch(Regexes.family1.rawValue).cleanTheTextOf(fmhBadBits)}
+    var preventiveCare: String {return chartData.simpleRegExMatch(Regexes.preventive.rawValue).cleanTheTextOf(preventiveBadBits)}
+    var psh: String {return chartData.simpleRegExMatch(Regexes.psh.rawValue).cleanTheTextOf(pshBadBits)}
+    var pmh: String {return chartData.simpleRegExMatch(Regexes.pmh.rawValue).cleanTheTextOf(pmhBadBits)}
+    
+    enum Regexes:String {
+        case social = "(?s)(Social history).*((?<=)Past medical history)"
+        case family1 = "(?s)(Family health history).*(Preventive care)"
+        case family2 = "(?s)(Family health history).*(Social history)"
+        case nutrition1 = "(?s)(Nutrition history).*((?<=)Developmental history)"
+        case nutrition2 = "(?s)(Nutrition history).*((?<=)Allergies\\n)"
+        case diagnoses = "(?s)Diagnoses.*Social history*?\\s(?=\\nSmoking status*?\\s\\n)"
+        case medications = "(?s)(Medications).*(Encounters)"
+        case allergies = "(?s)(\nAllergies\n).*(Medications)"
+        case pmh = "(?s)(Ongoing medical problems).*(Family health history)"
+        case psh = "(?s)(Major events).*(Ongoing medical problems)"
+        case preventive = "(?s)(Preventive care).*((?<=)Social history)"
+    }
+    
+    //Get the name, age, and DOB from the text
+   private func nameAgeDOB(_ theText: String) -> (String, String, String){
+        var ptName = ""
+        var ptAge = ""
+        var ptDOB = ""
+        let theSplitText = theText.components(separatedBy: "\n")
+        
+        var lineCount = 0
+        if !theSplitText.isEmpty {
+            for currentLine in theSplitText {
+                if currentLine.range(of: "PRN: ") != nil {
+                    let ageLine = theSplitText[lineCount + 1]
+                    ptName = theSplitText[lineCount - 1]
+                    ptAge = ageLine.simpleRegExMatch("^\\d*")
+                } else if currentLine.hasPrefix("DOB: "){
+                    let dobLine = currentLine
+                    ptDOB = dobLine.simpleRegExMatch("\\d./\\d./\\d*")
+                }
+                lineCount += 1
+            }
+        }
+        return (ptName, ptAge, ptDOB)
+    }
+    
+    //Clean extraneous text from the sections
+    private func cleanTheSections(_ theSection:String, badBits:[String]) -> String {
+        //var cleanedText = theSection.removeWhiteSpace()
+        let textArray = theSection.split(separator: "\n")
+        print(textArray)
+        var cleanedText = textArray.filter {$0.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines) != ""}.joined(separator: "\n")
+        
+        for theBit in badBits {
+            cleanedText = cleanedText.replacingOccurrences(of: theBit, with: "")
+        }
+        cleanedText = cleanedText.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        return cleanedText
+    }
+}
+
+//Parse a string containing a full name into it's components and returns
+//the version of the name we use to label files
+func getFileLabellingName(_ name: String) -> String {
+    var fileLabellingName = String()
+    var ptFirstName = ""
+    var ptLastName = ""
+    var ptMiddleName = ""
+    var ptExtraName = ""
+    let extraNameBits = ["Sr", "Jr", "II", "III", "IV", "MD"]
+    
+    func checkForMatchInSets(_ arrayToCheckIn: [String], arrayToCheckFor: [String]) -> Bool {
+        var result = false
+        for item in arrayToCheckIn {
+            if arrayToCheckFor.contains(item) {
+                result = true
+                break
+            }
+        }
+        return result
+    }
+    
+    let nameComponents = name.components(separatedBy: " ")
+    
+    let extraBitsCheck = checkForMatchInSets(nameComponents, arrayToCheckFor: extraNameBits)
+    
+    if extraBitsCheck == true {
+        ptLastName = nameComponents[nameComponents.count-2]
+        ptExtraName = nameComponents[nameComponents.count-1]
+    } else {
+        ptLastName = nameComponents[nameComponents.count-1]
+        ptExtraName = ""
+    }
+    
+    if nameComponents.count > 2 {
+        if nameComponents[nameComponents.count - 2] == "Van" {
+            ptLastName = "Van " + ptLastName
+        }
+    }
+    
+    //Get first name
+    ptFirstName = nameComponents[0]
+    
+    //Get middle name
+    if (nameComponents.count == 3 && extraBitsCheck == true) || nameComponents.count < 3 {
+        ptMiddleName = ""
+    } else {
+        ptMiddleName = nameComponents[1]
+    }
+    
+    fileLabellingName = "\(ptLastName)\(ptFirstName)\(ptMiddleName)\(ptExtraName)"
+    let badNameBits = [" ", "-", "'", "(", ")", "\""]
+    for bit in badNameBits {
+        fileLabellingName = fileLabellingName.replacingOccurrences(of: bit, with: "")
+    }
+    
+    return fileLabellingName
+}
