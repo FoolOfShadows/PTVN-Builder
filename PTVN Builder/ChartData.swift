d253cb361a1fabaaa15204708e0de03e7e29eaf1
Fool <foolofshadows@gmail.com>
2018-03-22 09:55:06 -0500

    Updated regex for Medications section

--- PTVN Builder/ChartData.swift
+++ PTVN Builder/ChartData.swift
@@ -36,7 +36,7 @@ struct ChartData {
         case nutrition1 = "(?s)(Nutrition history).*((?<=)Developmental history)"
         case nutrition2 = "(?s)(Nutrition history).*((?<=)Allergies\\n)"
         case diagnoses = "(?s)Diagnoses.*Social history*?\\s(?=\\nSmoking status*?\\s\\n)"
-        case medications = "(?s)(Medications).*(Encounters)"
+        case medications = "(?s)(Medications*\\s+?\\n).*(Encounters)"
         case allergies = "(?s)(\nAllergies\n).*(Medications)"
         case pmh = "(?s)(Ongoing medical problems).*(Family health history)"
         case psh = "(?s)(Major events).*(Ongoing medical problems)"
